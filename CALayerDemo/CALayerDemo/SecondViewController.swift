075bec41d5cff3f650434e33906470368e220c75
Wishell <wishell@bk.ru>
2018-12-17 22:38:56 +0300

    - initial commit added project

---
+++ CALayerDemo/CALayerDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CALayerDemo
+//
+//  Created by Ivan Akulov on 07/12/2016.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
---
+++ CALayerDemo/CALayerDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CALayerDemo
+//
+//  Created by Ivan Akulov on 07/12/2016.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
